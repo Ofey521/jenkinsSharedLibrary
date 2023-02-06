ebedce63dc92e4a3edee9c5c6de428304f518a59
Mateusz Janczura <mat.janczura@gmail.com>
2023-02-06 23:40:20 +0100

    refaktor

--- vars/config.groovy
+++ vars/config.groovy
@@ -1,13 +1,8 @@
 def call(Map param = [:]) {
     configData = readYaml file: "jenkins_env_conf.yml"
 
-    if (param.version.matches('')){
-        param.version = configData.version   
-    }
-
-    if (param.path.matches('')){
-        param.path = configData.path
-    }
+    if (param.version.matches('')) param.version = configData.version
+    if (param.path.matches('')) param.path = configData.path
 
     return [path: param.path, version: param.version]
 }
