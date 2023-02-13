02fe159dfc20108e8e63efc432d02d6843c66d56
Mateusz Janczura <mat.janczura@gmail.com>
2023-02-13 19:50:18 +0100

    path z configFile

--- vars/config.groovy
+++ vars/config.groovy
@@ -2,10 +2,10 @@ def call(Map param = [:]) {
     configData = readYaml file: "jenkins_env_conf.yml"
 
     if (param.version.matches('')) param.version = configData.version
-    if (param.path.matches('')) param.path = configData.path
     if (param.nVersion.matches('')) param.nVersion = configData.nVersion
+    
     if (param.nVersion == null) error 'nVersion not found in jenkins_env_conf'
-    if (param.path == null) error 'path not found in jenkins_env_conf'
+    if (configData.path == null) error 'path not found in jenkins_env_conf'
 
-    return [path: param.path, version: param.version, nVersion: param.nVersion]
+    return [path: configData.path, version: param.version, nVersion: param.nVersion]
 }
