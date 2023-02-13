5c3bbe254cc4baa31f5daec83cb3008b1c7bc69a
Mateusz Janczura <mat.janczura@gmail.com>
2023-02-13 18:46:08 +0100

    valid nversion

--- vars/config.groovy
+++ vars/config.groovy
@@ -3,6 +3,8 @@ def call(Map param = [:]) {
 
     if (param.version.matches('')) param.version = configData.version
     if (param.path.matches('')) param.path = configData.path
+    if (param.nVersion.matches('')) param.nVersion = configData.nVersion
+    if (param.nVersion == null) error 'nVersion not found in jenkins_env_conf'
 
-    return [path: param.path, version: param.version]
+    return [path: param.path, version: param.version, nVersion: param.nVersion]
 }
